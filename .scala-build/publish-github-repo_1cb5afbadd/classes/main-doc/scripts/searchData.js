eff6b948027fe042af686c52d9c5276cc529dc01
Maciej Gajek <mgajek@virtuslab.com>
2023-08-23 15:53:50 +0200

    First

---
+++ .scala-build/publish-github-repo_1cb5afbadd/classes/main-doc/scripts/searchData.js
@@ -0,0 +1,3 @@
+pages = [{"l":"index.html#","e":false,"i":"","n":"root","t":"root","d":"","k":"static","x":""},
+{"l":"https://scala-lang.org/api/3.x/<empty>/Foo.html","e":true,"i":"","n":"Foo","t":"Foo(weirdName: String)","d":"","k":"class","x":""},
+{"l":"https://scala-lang.org/api/3.x/<empty>/Foo.html#weirdDef-0","e":true,"i":"","n":"weirdDef","t":"weirdDef: String","d":"Foo","k":"def","x":""}];
---
+++ Foo.scala
@@ -0,0 +1,3 @@
+class Foo(weirdName: String) {
+    def weirdDef = weirdName  * 5
+}
